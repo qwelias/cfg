398f78a71dd2f102bdc6bd7e044ae2a3c25d9e94
Ilia Baryshnikov <qwelias@gmail.com>
2020-02-29 13:30:55 +0100

    feat: makeshift setup script

---
+++ script/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+echo 'Installing goodies'
+yay -S ttf-ubuntu-font-family xss-lock xsecurelock micro numix-square-icon-theme
+
+echo 'Setting GNOME Keyring unlock via PAM'
+sudo sh -c 'echo "\npassword    optional    pam_gnome_keyring.so" >> /etc/pam.d/passwd'
+sudo sh -c 'echo "\nauth       optional     pam_gnome_keyring.so\nsession    optional     pam_gnome_keyring.so auto_start" >> /etc/pam.d/login'
+
+echo 'Getting ZSH theme'
+sudo sh -c 'curl https://gist.githubusercontent.com/qwelias/e165b3f78c5ad6d8a61b093e0ef98d6b/raw > /usr/share/oh-my-zsh/themes/qwelias.zsh-theme'
+
+echo 'Installing GNOME Shell theme'
+yay -S autoconf automake inkscape gdk-pixbuf2 glib2 libxml2 pkgconf sassc parallel
+git clone https://github.com/qwelias/adapta-gtk-theme /tmp/adapta
+
+cd /tml/adapta
+./my-autogen.sh
+make
+sudo make install
