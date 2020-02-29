1af04551d2a984a799e55caaef087d22af362505
Ilia Baryshnikov <qwelias@gmail.com>
2020-02-29 13:53:03 +0100

    fix: setup linebreaks

--- script/setup.sh
+++ script/setup.sh
@@ -9,8 +9,8 @@ echo 'Installing goodies'
 yay -S ttf-ubuntu-font-family xss-lock xsecurelock micro-bin numix-square-icon-theme google-chrome
 
 echo 'Setting GNOME Keyring unlock via PAM'
-sudo sh -c 'echo "\npassword    optional    pam_gnome_keyring.so" >> /etc/pam.d/passwd'
-sudo sh -c 'echo "\nauth       optional     pam_gnome_keyring.so\nsession    optional     pam_gnome_keyring.so auto_start" >> /etc/pam.d/login'
+sudo sh -c 'printf "\npassword    optional    pam_gnome_keyring.so" >> /etc/pam.d/passwd'
+sudo sh -c 'printf "\nauth       optional     pam_gnome_keyring.so\nsession    optional     pam_gnome_keyring.so auto_start" >> /etc/pam.d/login'
 
 echo 'Getting ZSH theme'
 sudo sh -c 'curl https://gist.githubusercontent.com/qwelias/e165b3f78c5ad6d8a61b093e0ef98d6b/raw > /usr/share/oh-my-zsh/themes/qwelias.zsh-theme'
@@ -19,7 +19,7 @@ echo 'Installing GNOME Shell theme'
 yay -S autoconf automake inkscape gdk-pixbuf2 glib2 libxml2 pkgconf sassc parallel
 git clone https://github.com/qwelias/adapta-gtk-theme /tmp/adapta
 
-cd /tml/adapta
+cd /tmp/adapta
 ./my-autogen.sh
 make
 sudo make install
