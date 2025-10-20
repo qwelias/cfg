c0b7e0d402332ffca2d86a553747d7548ddef0ca
Ilia Baryshnikov <qwelias@gmail.com>
2025-10-20 06:10:26 +0500

    upd

--- script/setup.sh
+++ script/setup.sh
@@ -85,8 +85,10 @@ setup_gnome () {
     gsettings set org.gnome.desktop.interface font-name 'Ubuntu Light 13'
     gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono 12'
     gsettings set org.gnome.desktop.input-sources per-window true
-    gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
     gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>space']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Alt>space']"
 }
 
 setup_keyring () {
